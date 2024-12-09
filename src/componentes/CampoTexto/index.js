89c7dc4669ea0ddb75faf96b910e5a4082a9ffc9
ingridk12 <ingridkvegao@gmail.com>
2024-12-09 20:26:11 +0000

    se condicionaron los campos requeridos

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,14 @@
-import "./CampoTexto.css"
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
-    console.log("Datos: ", props.titulo)
-    const placeholderModificado = `${props.placeholder}...`
-    return <div className="campo-texto">
-        <label>{props.titulo}</label>
-        <input placeholder={placeholderModificado} />
+  console.log("Datos: ", props);
+  const placeholderModificado = `${props.placeholder}...`;
+  return (
+    <div className="campo-texto">
+      <label>{props.titulo}</label>
+      <input placeholder={placeholderModificado} required={props.required} />
     </div>
-}
+  );
+};
 
-export default CampoTexto
+export default CampoTexto;
