bcaf61a90fd86bd9bef3308675e8558790a2a64e
ingridk12 <ingridkvegao@gmail.com>
2024-12-11 20:44:39 +0000

    creando equipos

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 import Header from "./componentes/Header/Header";
 import Formulario from "./componentes/Formulario/Formulario";
 import MiOrg from "./componentes/MiOrg";
+import Equipo from "./componentes/Equipo";
 function App() {
-  const [mostrarFormulario, actualizarMostrar] = useState(true);
+  const [mostrarFormulario, actualizarMostrar] = useState(false);
   //Ternario -->condicion ? seMuestra - noSeMuestra
   const cambiarMostrar = () => {
     actualizarMostrar(!mostrarFormulario);
@@ -15,6 +16,13 @@ function App() {
       {mostrarFormulario === true ? <Formulario /> : <div></div>}
 
       <MiOrg cambiarMostrar={cambiarMostrar} />
+      <Equipo equipo={"Programación"} />
+      <Equipo equipo={"Front End"} />
+      <Equipo equipo={"Data Science"} />
+      <Equipo equipo={"Devops"} />
+      <Equipo equipo={"UX y Diseño"} />
+      <Equipo equipo={"Móvil"} />
+      <Equipo equipo={"Innovación y  Gestión"} />
     </div>
   );
 }
